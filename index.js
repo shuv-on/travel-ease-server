e59816ec70d69a96ae162dbaae026f5621350de7
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-11-25 19:33:28 +0600

    added vercel config

--- index.js
+++ index.js
@@ -13,50 +13,55 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tk04nnw.mongodb.net/?appName=Cluster0&retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 app.get('/', (req, res) => {
     res.send('Travel ease server is running')
 })
 
-async function run (){
-    try{
+async function run() {
+    try {
         await client.connect();
 
         const db = client.db("travel-ease");
         const carCollection = db.collection("cars");
 
-     
-        app.get('/cars', async(req, res) => {
-            const cursor = carCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
+
+        app.get('/cars', async (req, res) => {
+            const limit = parseInt(req.query.limit);
+            if (limit) {
+                const result = await carCollection.find().sort({ _id: -1 }).limit(limit).toArray();
+                res.send(result);
+            } else {
+                const result = await carCollection.find().toArray();
+                res.send(result);
+            }
         })
 
-      
-        app.post('/cars', async(req, res) => {
+
+        app.post('/cars', async (req, res) => {
             const vehicle = req.body;
-            console.log("Data received:", vehicle); 
+            console.log("Data received:", vehicle);
             const result = await carCollection.insertOne(vehicle);
             res.send(result);
         })
-        
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     }
     finally {
-        
+
     }
 }
 
 run().catch(console.dir);
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Travel ease server is running on port: ${port}`)
 })
